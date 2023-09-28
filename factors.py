7c005c7536091d3fbee338d2849af4653fe9afbb
RewanAbdulkariem <rewan.khaled21@gmail.com>
2023-09-28 21:26:06 +0300

    fix style

--- factors.py
+++ factors.py
@@ -2,11 +2,13 @@
 import sys
 import math
 
+
 def factorize_number(n):
     for i in range(2, n):
         if n % i == 0:
             print("{:d}={:d}*{:d}".format(n, n // i, i))
-            break;
+            break
+
 
 def main(file_path):
     try:
@@ -18,6 +20,7 @@ def main(file_path):
         print(f"Error: can't open file {filename}")
         sys.exit(1)
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: factors <file>")
@@ -25,4 +28,3 @@ if __name__ == "__main__":
 
     file_path = sys.argv[1]
     main(file_path)
-
